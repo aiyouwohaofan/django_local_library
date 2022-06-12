1966209f098a63cba28981cf5c01cb99a83b9df4
sunzzzzzt3 <sunzzzzzt3@gmail.com>
2022-06-12 09:10:41 +0800

    1

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-=*@!v5-7w3)igu
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
